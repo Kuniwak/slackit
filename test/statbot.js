b0f49b7b8d6af4eebc2ae2b8d48212d6045e294b
OrgaChem <orga.chem.job@gmail.com>
2014-04-23 19:51:14 +0900

    Add tests for constructing

--- test/statbot.js
+++ test/statbot.js
@@ -3,4 +3,21 @@ var request = require('supertest');
 var Statbot = require('../');
 
 describe('Statbot', function() {
+  describe('.constructor', function() {
+    it('should throw an exception when given no options', function() {
+      var statbot = new Statbot();
+    });
+
+    it('should throw an exception when given invalid options', function() {
+      var statbot = new Statbot({});
+    });
+
+    it('should construct the bot', function() {
+      var statbot = new Statbot({
+        channel: 'orgachem',
+        username: 'test',
+        incomingHookToken: 'AAAAAAAAAAAAAAAAAAAAAAAA'
+      });
+    });
+  });
 });
