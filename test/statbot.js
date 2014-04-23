6d486e1ff4b93411957bfbdb621b0a8dc6981bb1
OrgaChem <orga.chem.job@gmail.com>
2014-04-23 15:01:02 +0900

    Redesign api

--- test/statbot.js
+++ test/statbot.js
@@ -1,18 +1,6 @@
 var expect = require('chai').expect;
+var request = require('supertest');
 var Statbot = require('../');
 
 describe('Statbot', function() {
-  describe('#start', function() {
-    it('should start the http server.', function() {
-      var statbot = new Statbot();
-      statbot.start();
-    });
-  });
-
-  describe('#stop', function() {
-    it('should stop the http server.', function() {
-      var statbot = new Statbot();
-      statbot.stop();
-    });
-  });
 });
