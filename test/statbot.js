e6c59f1edec540802dc89c6b7fefc846a72abe68
OrgaChem <orga.chem.job@gmail.com>
2014-04-23 13:47:39 +0900

    Add a test for Statbot#stop

--- test/statbot.js
+++ test/statbot.js
@@ -3,9 +3,16 @@ var Statbot = require('../');
 
 describe('Statbot', function() {
   describe('#start', function() {
-    it('should be start the http server.', function() {
+    it('should start the http server.', function() {
       var statbot = new Statbot();
       statbot.start();
+    });
+  });
+
+  describe('#stop', function() {
+    it('should stop the http server.', function() {
+      var statbot = new Statbot();
+      statbot.stop();
     });
   });
 });
