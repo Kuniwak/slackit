cd384550a134fadbe0916eb070cfebb063348bb2
OrgaChem <orga.chem.job@gmail.com>
2014-04-24 15:52:01 +0900

    Redegisn tests for the constructor

--- test/statbot.js
+++ test/statbot.js
@@ -3,21 +3,30 @@ var request = require('supertest');
 var Statbot = require('../');
 
 describe('Statbot', function() {
+  var VALID_OPTIONS = {
+    teamname: 'orgachem',
+    channel: 'general',
+    username: 'test',
+    incomingHookToken: 'AAAAAAAAAAAAAAAAAAAAAAAA'
+  };
+
+  var INVALID_OPTIONS = {};
+
   describe('.constructor', function() {
     it('should throw an exception when given no options', function() {
-      var statbot = new Statbot();
+      expect(function() {
+        new Statbot();
+      }).to.throw(Error);
     });
 
     it('should throw an exception when given invalid options', function() {
-      var statbot = new Statbot({});
+      expect(function() {
+        new Statbot(INVALID_OPTIONS);
+      }).to.throw(Error);
     });
 
-    it('should construct the bot', function() {
-      var statbot = new Statbot({
-        channel: 'orgachem',
-        username: 'test',
-        incomingHookToken: 'AAAAAAAAAAAAAAAAAAAAAAAA'
-      });
+    it('should construct the bot with no mechanisms', function() {
+      new Statbot(VALID_OPTIONS);
     });
   });
-});
+})
